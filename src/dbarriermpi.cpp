049a786308c90dd2a40991cafdf5f9fd02a6f6a8
Aman Mangal <mangalaman93@gmail.com>
2015-03-09 18:24:25 -0400

    fixed dbarrier

--- src/dbarriermpi.cpp
+++ src/dbarriermpi.cpp
@@ -16,6 +16,9 @@ void DBarrierMPI::barrier() {
   for(int pow2=1; pow2<num_procs; pow2=pow2<<1) {
     int dst = (rank+pow2) % num_procs;
     int src = ((rank-pow2) % num_procs);
+    if(src < 0) {
+      src += num_procs;
+    }
     MPI_Send(&msg, 1, MPI_CHAR, dst, 0, MPI_COMM_WORLD);
     MPI_Recv(&msg, 1, MPI_CHAR, src, 0, MPI_COMM_WORLD, &stat);
   }
